9df5e93c691d499ef4832126a02a03aba8338fad
Ethan Brierley <ethanboxx@gmail.com>
2020-07-31 22:33:19 +0100

    WE HAVE SOUND!!!

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,64 @@
 use seed::prelude::*;
 use seed::*;
+use web_sys::AudioContext;
+use web_sys::OscillatorType;
 
 use seed_style::px; // almost always want seed-style px instead of seed px
 use seed_style::*;
 
-mod global_styles;
 mod app;
+mod global_styles;
 
 //
 //  Model, Msg, Update, init(), and start()
 //  ---------------------------------------
 
-pub struct Model {}
+pub struct Model {
+    //oscillator: OscillatorNode,
+}
 
 // In aps that make use of conditional rendering on breakpoints we We just need one Msg
 // in order to handle a WindowResized event.
-#[derive(Clone,Debug)]
+#[derive(Clone, Debug)]
 pub enum Msg {
     ProduceSound,
 }
 
 fn update(msg: Msg, _model: &mut Model, _orders: &mut impl Orders<Msg>) {
-    log!(msg);    // always worth logging the message in development for debug purposes.
+    log!(msg);
+
+    match msg {
+        Msg::ProduceSound => {
+            make_a_sound().unwrap();
+        }
+    }
+}
+
+fn make_a_sound() -> Result<(), JsValue> {
+    let audio_context = AudioContext::new()?;
+
+    let oscillator = audio_context.create_oscillator()?;
+    oscillator.set_type(OscillatorType::Sine);
+    oscillator.frequency().set_value(840.);
+
+    let gain = audio_context.create_gain()?;
+    oscillator.connect_with_audio_node(&gain)?;
+    gain.connect_with_audio_node(&audio_context.destination())?;
+
+    oscillator.start()?;
+    Ok(())
 }
 
 fn init(url: Url, orders: &mut impl Orders<Msg>) -> Model {
-    // allow app to access the user theme, plus seed style presets.
-    // app_themes().set(vec![user_theme(), default_color_theme()]);
-    // themes values can now be used in the global style init
+    orders.send_msg(Msg::ProduceSound);
+
     global_styles::global_init();
     Model {}
 }
 
 #[wasm_bindgen(start)]
 pub fn start() {
-    let _app  = App::start("app", init, update, view);
+    let _app = App::start("app", init, update, view);
 }
 
 //  View Entry Here, Sets up theme access, two themes are allowed access
@@ -45,9 +69,6 @@ pub fn start() {
 pub fn view(model: &Model) -> Node<Msg> {
     div![
         s().display_flex().flex_direction_row(),
-        div![
-            s().width(px(200)).flex_none(),
-            "Hello World"
-        ],
+        div![s().width(px(200)).flex_none(), "Hello World"],
     ]
 }
