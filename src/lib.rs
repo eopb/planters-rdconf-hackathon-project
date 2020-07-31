47f58b746e3ed609fd763626111033c8de8b8a64
Giles Cope <gilescope@gmail.com>
2020-08-01 00:32:29 +0100

    select note

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
 use seed::prelude::*;
 use seed::*;
-use web_sys::AudioContext;
-use web_sys::OscillatorType;
 
 use seed_style::px; // almost always want seed-style px instead of seed px
 use seed_style::{pc, *};
+use web_sys::{HtmlCanvasElement, HtmlElement};
 
 mod app;
 mod global_styles;
@@ -17,6 +16,7 @@ use sound::{Sound, SoundBuilder};
 
 pub struct Model {
     sound: Sound,
+    canvas: ElRef<HtmlCanvasElement>,
 }
 
 // In aps that make use of conditional rendering on breakpoints we We just need one Msg
@@ -25,6 +25,7 @@ pub struct Model {
 pub enum Msg {
     ProduceSound,
     StopSound,
+    Click(i32, i32),
 }
 
 fn update(msg: Msg, model: &mut Model, _orders: &mut impl Orders<Msg>) {
@@ -37,16 +38,26 @@ fn update(msg: Msg, model: &mut Model, _orders: &mut impl Orders<Msg>) {
         Msg::StopSound => {
             model.sound.pause();
         }
+        Msg::Click(x, y) => {
+            let canvas_el = model.canvas.get().unwrap();
+            let width = canvas_el.width() as f32;
+            let height = canvas_el.height() as f32;
+
+            let el: HtmlElement = canvas_el.into();
+            let freq = ((x - el.offset_left()) as f32 * 11_00. / width) as f32;
+            let vol = ((y - el.offset_top()) as f32 * 10. / height) as f32;
+            model.sound = SoundBuilder::new().gain(vol).freq(freq).build().unwrap();
+        }
     }
 }
 
 fn init(url: Url, orders: &mut impl Orders<Msg>) -> Model {
     global_styles::global_init();
-    let sound = SoundBuilder::new()
-        .freq(500.)
-        .build()
-        .unwrap();
-    Model { sound }
+    let sound = SoundBuilder::new().freq(500.).build().unwrap();
+    Model {
+        sound,
+        canvas: ElRef::<HtmlCanvasElement>::default(),
+    }
 }
 
 #[wasm_bindgen(start)]
@@ -66,7 +77,13 @@ pub fn view(model: &Model) -> Node<Msg> {
             .height(pc(100))
             .width(pc(100)),
         div![
-            "hello world",
+            canvas![
+                el_ref(&model.canvas),
+                style![
+                    St::Border => "1px solid black",
+                ],
+                mouse_ev(Ev::MouseDown, |event| Msg::Click(event.x(), event.y()))
+            ],
             // Where to put the canvas
         ],
         div![
