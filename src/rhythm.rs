032f2d35d89c73f6732ae3ffde3cfab8e4290080
Ethan Brierley <ethanboxx@gmail.com>
2020-08-01 10:18:17 +0100

    use an array

--- src/rhythm.rs
+++ src/rhythm.rs
@@ -1,42 +1,28 @@
 //! Types for creating repeating rhythms.
 
 /// A rhythm is a collection off notes and pauses to make a pattern.
-struct Rhythm {
-    inner: Vec<Beat>,
-}
+struct Rhythm([Beat; 48]);
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Eq, PartialEq)]
 enum Beat {
-    Play(f64),
-    Pause(f64),
+    Play,
+    Pause,
 }
 
 impl Beat {
-    fn len(self) -> f64 {
-        match self {
-            Self::Play(x) => x,
-            Self::Pause(x) => x,
-        }
+    fn is_playing(self) -> bool {
+        self == Self::Play
+    }
+    fn is_paused(self) -> bool {
+        self == Self::Pause
     }
 }
 
 impl Rhythm {
-    fn new(inner: Vec<Beat>) -> Self {
-        Self { inner }
-    }
     /// The set of beats we want to offer the user.
     fn standard() -> Vec<Self> {
-        vec![
-            Self::new(vec![Beat::Play(1.), Beat::Pause(1.)]),
-            Self::new(vec![Beat::Play(1.), Beat::Pause(1.)]),
-            Self::new(vec![Beat::Play(1.), Beat::Pause(1.)]),
-            Self::new(vec![Beat::Play(1.), Beat::Pause(1.)]),
-            Self::new(vec![Beat::Play(1.), Beat::Pause(1.)]),
-            Self::new(vec![Beat::Play(1.), Beat::Pause(1.)]),
-        ]
+        vec![Self([Beat::Play; 48])]
     }
     /// Draw a visual rep of a rhythm to a canvas using dots dashes and breaks.
-    fn draw(self) {
-        let len: f64 = self.inner.into_iter().map(Beat::len).sum();
-    }
+    fn draw(self) {}
 }
