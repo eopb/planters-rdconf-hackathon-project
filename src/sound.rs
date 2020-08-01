e4713046f64ed37d10a59ca33c4847de9b6271dc
David Purdum <purdum41@gmail.com>
2020-08-01 10:59:39 -0400

    Pull new changes

--- src/sound.rs
+++ src/sound.rs
@@ -1,10 +1,23 @@
+use seed::prelude::JsValue;
 use web_sys::OscillatorType;
 use web_sys::{AudioContext, GainNode, OscillatorNode};
-use seed::prelude::JsValue;
+
+#[derive(Debug, Clone)]
+pub enum SoundStatus {
+    Unplayed,
+    Played,
+}
+
+impl Default for SoundStatus {
+    fn default() -> Self {
+        SoundStatus::Unplayed
+    }
+}
 
 #[derive(Clone, Debug, Default)]
 pub struct Sound {
     tones: Vec<Tone>,
+    status: SoundStatus,
 }
 
 impl Sound {
@@ -13,7 +26,10 @@ impl Sound {
     }
 
     pub fn from_tones(tones: Vec<Tone>) -> Self {
-        Self { tones }
+        Self {
+            tones,
+            status: SoundStatus::Unplayed,
+        }
     }
 
     pub fn add_tone(mut self, s: Tone) -> Self {
@@ -21,7 +37,8 @@ impl Sound {
         self
     }
 
-    pub fn play(&self) {
+    pub fn play(&mut self) {
+        self.status = SoundStatus::Played;
         for tone in &self.tones {
             tone.play();
         }
@@ -49,13 +66,19 @@ pub struct Tone {
 }
 
 impl Tone {
-    fn play(&self) {
+    pub fn play(&self) {
         self.context.resume().unwrap(); // Fix for Chromium
-        self.gain.gain().set_value(self.gain_val);
+        self.gain
+            .gain() //.set_value(self.gain_val);
+            .set_target_at_time(self.gain_val, self.context.current_time(), 0.015)
+            .unwrap();
     }
 
-    fn pause(&self) {
-        self.gain.gain().set_value(0.0);
+    pub fn pause(&self) {
+        self.gain
+            .gain()
+            .set_target_at_time(0.0, self.context.current_time(), 0.015)
+            .unwrap();
     }
 
     fn half_gain(&self) {
@@ -90,25 +113,67 @@ impl ToneBuilder {
     pub fn gain(self, gain: f32) -> Self {
         ToneBuilder { gain, ..self }
     }
-    
+
     pub fn build(self) -> Result<Tone, JsValue> {
         let context = AudioContext::new()?;
 
         let gain = context.create_gain()?;
-        gain.gain().set_value(0.);
+        gain.gain().set_value(0.0);
         gain.connect_with_audio_node(&context.destination())?;
 
         let oscillator = context.create_oscillator()?;
         oscillator.set_type(self.osc_type);
         oscillator.frequency().set_value(self.freq);
         oscillator.connect_with_audio_node(&gain)?;
+
+        context.resume()?;
         oscillator.start()?;
 
-        Ok(Tone { 
-            oscillator, 
-            gain, 
+        Ok(Tone {
+            oscillator,
+            gain,
             context,
             gain_val: self.gain,
+        })
+    }
+
+    pub fn to_sound(self) -> Result<Sound, JsValue> {
+        let tone = self.build()?;
+        Ok(Sound::from_tones(vec![tone]))
+    }
+}
+
+struct SSound {
+    oscillator: OscillatorNode,
+    gain: GainNode,
+    context: AudioContext,
+    gain_val: f32,
+}
+
+impl Default for SSound {
+    fn default() -> Self {
+        Self::build().unwrap()
+    }
+}
+
+impl SSound {
+    fn build() -> Result<Self, JsValue> {
+        let context = AudioContext::new()?;
+
+        let gain = context.create_gain()?;
+        gain.gain().set_value(0.0);
+        gain.connect_with_audio_node(&context.destination())?;
+
+        let oscillator = context.create_oscillator()?;
+        oscillator.set_type(self.osc_type);
+        oscillator.frequency().set_value(self.freq);
+        oscillator.connect_with_audio_node(&gain)?;
+
+        Ok(SSound {
+            oscillator,
+            gain,
+            context,
+            gain_val,
         })
     }
 }
